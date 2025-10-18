addcf1a69006456fe4df007e9ad053d0702aeea4
Isaac <isaacljgilbert@gmail.com>
2025-10-18 22:00:12 +0100

    initial set up

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#TODO: Everything basically (dot files, program list, funcs to install and manage packages, script to assemble funcs)
+
+### SET UP ###
+
+### FUNCS ###
+
+welcome() {
+    whiptail --title "hiii" \
+        --msgbox "Test dialog box hiii whats good" 8 78
+}
+
+### SCRIPT ###
