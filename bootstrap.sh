3af1f4a1d7742ebaa6e1571a9d84280ec5d5981c
Isaac <isaacljgilbert@gmail.com>
2025-10-19 21:47:47 +0100

    start of program list

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #TODO: Everything basically (dot files, program list, funcs to install and manage packages, script to assemble funcs)
+#TODO: Test out different Wayland set ups on old laptop in preparation for new
 
 ### SET UP ###
 
